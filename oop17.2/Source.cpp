7a450f058271c78af6b6f043de411db223e1d434
nameArkeniAlastorgit config --global user.email <parkerosnova@gmail.com>
2025-11-04 20:41:26 +0200

    gg

--- oop17.2/Source.cpp
+++ oop17.2/Source.cpp
@@ -15,26 +15,45 @@ public:
 		surname = s;
 		age = a;
 	}
-	void Print()
-	{
-		cout << "Name: " << name << "Surname: " << surname << "\tAge: " << age << endl;
-	}
-	void Input()
-	{
-		cout << "Enter Name: ";
-		cin >> name;
 
-		cout << "Enter Surname: ";
-		cin >> surname;
+    void SaveFile()
+    {
+        ofstream file("student.txt");
+        if (!file.is_open())
+        {
+            cout << "Error opening file!" << endl;
+            return;
+        }
 
-		cout << "Enter Age: ";
-		cin >> age;
-	}
+        file << name << endl;
+        file << surname << endl;
+        file << age << endl;
 
+        file.close();
+        cout << "Data saved successfully!" << endl;
+    }
 
+    void LoadFile()
+    {
+        ifstream file("student.txt");
+        if (!file.is_open())
+        {
+            cout << "Error opening file!" << endl;
+            return;
+        }
+
+        getline(file, name);
+        getline(file, surname);
+        file >> age;
+
+        file.close();
+        cout << "Data loaded successfully!" << endl;
+    }
 };
 
 int main()
 {
-	Student obj1("Ivan", "Petrov", 24)
+	Student obj1("Ivan", "Petrov", 24);
+    obj1.SaveFile();
+    obj1.LoadFile();
 }
